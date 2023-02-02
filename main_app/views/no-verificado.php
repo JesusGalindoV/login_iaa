17f6e6505267cda2146f566f3eaba7e50376dbb0
JesusGalindo <janthonygalindov@gmail.com>
2023-02-02 12:54:35 -0700

    changes

--- main_app/views/no-verificado.php
+++ main_app/views/no-verificado.php
@@ -4,11 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="../../img/yoxd.jpg" type="image/jpg"> 
+    <link rel="Stylesheet" href="../../css/master.css">
+
     <title>Document</title>
 </head>
 <body>
 
-<h1>NO ESTAS VERIFICADO</h1>
+<h3 class="no-verified">NO ESTAS VERIFICADO</h3>
+
+
+    <section class="home">
+        <div class="login-box">
+            <img class="avatar" src="../../img/yoxd.jpg" alt="Jesús Antonio">
+            <h1>Check code</h1><br>
+            <form action="POST" id="formlg">
+                
+                <label for="codigo">Code:</label>
+                <input type="number" name="codigo" placeholder="0000" required>
+                
+                <input class="botonlg" type="submit" value="check code">
+            
+            </form>
+        </div>
+    </section>
+
 
 </body>
 </html>
